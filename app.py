fbac45f39030146fa4fb48274250f7c7099c1902
projetopi01 <166462433+projetopi01@users.noreply.github.com>
2024-05-14 11:00:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    if request.method == 'POST':
-        
-        return render_template('success.html', nome=request.form['nome'])
-
-
-@app.route('/submit_all', methods=['POST'])
-def submit_all():
-    if request.method == 'POST':
-       
+    nome = request.form.get('nome')
+    if nome:
+        return render_template('success.html', nome=nome)
+    else:
         return render_template('success.html')
 
 if __name__ == '__main__':
