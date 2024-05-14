2c91ab913594663200cbd5442d4f8258c25881f8
projetopi01 <166462433+projetopi01@users.noreply.github.com>
2024-05-14 14:50:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Credenciais de usuário (apenas para demonstração, não use hardcoded em um ambiente de produção)
+USUARIO = 'usuario'
+SENHA = 'senha'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/submit_login', methods=['POST'])
+def submit_login():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    if usuario == USUARIO and senha == SENHA:
+        # Se as credenciais estiverem corretas, redirecione para a página principal
+        return redirect(url_for('index'))
+    else:
+        # Se as credenciais estiverem incorretas, redirecione de volta para a página de login
+        return redirect(url_for('login'))
 
 @app.route('/submit', methods=['POST'])
 def submit():
